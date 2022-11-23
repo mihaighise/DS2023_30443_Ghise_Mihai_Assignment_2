64f04f5c1e236e2cfd06aa451d118573cd8955d2
mihaighise <mihaighise1@gmail.com>
2022-11-23 11:15:16 +0200

    initial files

---
+++ src/test/java/com/utcn/assignment2/Assignment2/Assignment2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utcn.assignment2.Assignment2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
